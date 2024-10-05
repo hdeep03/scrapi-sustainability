c89fe173f5edc031fd5b396e404cafe2a9ab433a
hdeep03 <hdeep03@gmail.com>
2024-10-05 19:32:57 -0400

    Add client scaffold

--- core/brower.py
+++ core/brower.py
@@ -1,10 +1,38 @@
-from playwright.async_api import Page
+import asyncio
+import logging
+
+from playwright.async_api import Page, async_playwright
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+logger = logging.getLogger(__name__)
 
 async def go_to_url(page: Page, url: str):
     try:
+        logger.info(f"Navigated to {url}")
         await page.goto(url, wait_until="domcontentloaded")
     except TimeoutError:
-        print(f"Timeout error while navigating to {url}")
-    except Exception as e:
-        print(f"An error occurred while navigating to {url}: {str(e)}")
+        logger.error(f"Timeout error while navigating to {url}")
 
+async def get_urls(page: Page):
+    urls = await page.evaluate("() => {return Array.from(document.querySelectorAll(\'a\')).map(a => a.href);}")
+    return urls
+
+async def get_full_coverage_screenshots(page: Page):
+    pass
+
+async def main():
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False)
+        page = await browser.new_page()
+        await go_to_url(page, "https://www.google.com")
+        urls = await get_urls(page)
+        print(urls)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+    
